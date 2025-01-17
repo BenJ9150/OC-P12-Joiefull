e7a5f2840479d1228ec98c9e1b8db86eb8ce9c73
benjamin <blefrancois@viapresse.com>
2025-01-17 11:55:13 +0100

    Project creation with gitignore

---
+++ Joiefull/Joiefull/JoiefullApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoiefullApp.swift
+//  Joiefull
+//
+//  Created by Benjamin LEFRANCOIS on 17/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct JoiefullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
